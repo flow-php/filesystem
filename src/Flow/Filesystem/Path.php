3b0e6fe5b0c1c1105f9378df148791ba6247839a
jay <917647288@qq.com>
2024-10-24 01:35:42 +0800

    Update Path.php
    only linux need add DIRECTORY_SEPARATOR at the beginning of path

--- src/Flow/Filesystem/Path.php
+++ src/Flow/Filesystem/Path.php
@@ -35,7 +35,7 @@ final class Path
 
         $path = \str_replace($scheme . '://', '', $uri);
 
-        if (!\str_starts_with($path, DIRECTORY_SEPARATOR)) {
+        if (DIRECTORY_SEPARATOR !='\\' && !\str_starts_with($path, DIRECTORY_SEPARATOR)) {
             $path = DIRECTORY_SEPARATOR . $path;
         }
 
