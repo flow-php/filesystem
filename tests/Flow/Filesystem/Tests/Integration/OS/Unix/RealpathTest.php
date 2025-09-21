414889d2d2943a5d88a3d64733ac9dfcf2922b70
norberttech <norbert@orzechowicz.pl>
2025-09-21 18:47:35 +0000

    Feature/filesystem windows support (#1855)

--- tests/Flow/Filesystem/Tests/Integration/OS/Unix/RealpathTest.php
+++ tests/Flow/Filesystem/Tests/Integration/OS/Unix/RealpathTest.php
@@ -2,20 +2,32 @@
 
 declare(strict_types=1);
 
-namespace Flow\Filesystem\Tests\Integration;
+namespace Flow\Filesystem\Tests\Integration\OS\Unix;
 
 use Flow\Filesystem\Path;
+use Flow\Filesystem\Tests\OperatingSystem;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 final class RealpathTest extends TestCase
 {
+    use OperatingSystem;
+
     public static function double_dots_paths() : \Generator
     {
         yield ['/path/../file.txt', '/file.txt'];
         yield ['/path/./file.txt', '/path/file.txt'];
         yield ['/path/..//../file.txt', '/file.txt'];
         yield ['/path/more/nested/..//../file.txt', '/path/file.txt'];
+    }
+
+    protected function setup() : void
+    {
+        parent::setUp();
+
+        if ($this->isWindows()) {
+            self::markTestSkipped('Realpath is not supported on Windows');
+        }
     }
 
     #[DataProvider('double_dots_paths')]
